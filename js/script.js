55bf8f64cf07b5506ba5d98503aa15b9eabd9eed
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-03-25 22:15:43 +0400

    Весь код полностью оптимизирован

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 const rates = {};
 
 // Элементы для отображение курса валют
-const elementUSD = document.querySelector('[data-value="USD"]'),
-    elementEUR = document.querySelector('[data-value="EUR"]'),
-    elementGBP = document.querySelector('[data-value="GBP"]');
+const elements = {
+    USD: document.querySelector('[data-value="USD"]'),
+    EUR: document.querySelector('[data-value="EUR"]'),
+    GBP: document.querySelector('[data-value="GBP"]')
+};
 
 // Элементы формы, ввод суммы, выбор валюты, поле с результатом
 const input = document.querySelector("#input"),
@@ -20,47 +22,30 @@ function getCurrencies() {
         .then(function (result) {
             return result.json();
         }).then(function (data) {
-            rates.USD = data.Valute.USD;
-            rates.EUR = data.Valute.EUR;
-            rates.GBP = data.Valute.GBP;
-            rates.UAH = data.Valute.UAH;
-            rates.BYN = data.Valute.BYN;
-            rates.AMD = data.Valute.AMD;
+            const values = ["USD", "EUR", "GBP", "BYN", "UAH", "AMD"];
+
+            values.forEach(function (value) {
+                rates[value] = data.Valute[value];
+            });
         }).then(() => showCurrencies());
 }
 
 // Функция отборажения валют на странице
 function showCurrencies() {
-    // Цвет для ифнормера USD
-    if (rates.USD.value > rates.USD.Previous) {
-        elementUSD.classList.add("top");
-    } else if (rates.USD.value === rates.USD.Previous) {
-        elementUSD.classList.add("evenly");
-    } else {
-        elementUSD.classList.add("bottom");
-    }
+    // Цвета для ифнормеров USD, EUR и GBP
+    const values = ["USD", "EUR", "GBP"];
 
-    // Цвет для ифнормера EUR 
-    if (rates.EUR.value > rates.EUR.Previous) {
-        elementEUR.classList.add("top");
-    } else if (rates.EUR.value === rates.EUR.Previous) {
-        elementEUR.classList.add("evenly");
-    } else {
-        elementEUR.classList.add("bottom");
-    }
+    values.forEach(function (value) {
+        if (rates[value].value > rates[value].Previous) {
+            elements[value].classList.add("top");
+        } else if (rates[value].value === rates[value].Previous) {
+            elements[value].classList.add("evenly");
+        } else {
+            elements[value].classList.add("bottom");
+        }
 
-    // Цвет для ифнормера GBP
-    if (rates.GBP.value > rates.GBP.Previous) {
-        elementGBP.classList.add("top");
-    } else if (rates.GBP.value === rates.GBP.Previous) {
-        elementGBP.classList.add("evenly");
-    } else {
-        elementGBP.classList.add("bottom");
-    }
-
-    elementUSD.textContent = rates.USD.Value.toFixed(2);
-    elementEUR.textContent = rates.EUR.Value.toFixed(2);
-    elementGBP.textContent = rates.GBP.Value.toFixed(2);
+        elements[value].textContent = rates[value].Value.toFixed(2);
+    });
 }
 
 getCurrencies();
@@ -70,27 +55,27 @@ setInterval(getCurrencies(), 10000);
 function convertValue() {
     if (give.value === "RUB") {
         if (this.id === "result") {
-            input.value = (result.value * rates[receive.value].Value).toFixed(2);
+            input.value = (result.value * rates[receive.valu].Value).toFixed(2);
         } else {
             result.value = (input.value / rates[receive.value].Value).toFixed(2);
         }
-    } else if (give.value === "UAH") {
-        if (this.id === "result") {
-            input.value = ((result.value / rates.UAH.Nominal) * rates[give.value].Value / rates[receive.value].Value).toFixed(2);
-        } else {
-            result.value = ((input.value / rates.UAH.Nominal) * rates[give.value].Value / rates[receive.value].Value).toFixed(2);
-        }
     } else if ((give.value === "BYN")) {
         if (this.id === "result") {
-            input.value = ((result.value * rates[give.value].Value) / rates[receive.value].Value).toFixed(2);
+            input.value = ((result.value / rates.BYN.Nominal * rates.BYN.Value) / rates[receive.value].Value).toFixed(2);
         } else {
-            result.value = ((input.value * rates[give.value].Value) / rates[receive.value].Value).toFixed(2);
+            result.value = ((input.value / rates.BYN.Nominal * rates.BYN.Value) / rates[receive.value].Value).toFixed(2);
+        }
+    } else if (give.value === "UAH") {
+        if (this.id === "result") {
+            input.value = ((result.value / rates.UAH.Nominal * rates.UAH.Value) / rates[receive.value].Value).toFixed(2);
+        } else {
+            result.value = ((input.value / rates.UAH.Nominal * rates.UAH.Value) / rates[receive.value].Value).toFixed(2);
         }
     } else if ((give.value === "AMD")) {
         if (this.id === "result") {
-            input.value = ((result.value / (rates.AMD.Nominal / rates.AMD.Value)) * rates[receive.value].Value).toFixed(2);
+            input.value = ((result.value / rates.AMD.Nominal * rates.AMD.Value) / rates[receive.value].Value).toFixed(2);
         } else {
-            result.value = ((input.value / (rates.AMD.Nominal / rates.AMD.Value)) / rates[receive.value].Value).toFixed(2);
+            result.value = ((input.value / rates.AMD.Nominal * rates.AMD.Value) / rates[receive.value].Value).toFixed(2);
         }
     }
 }
